5affc0a6166a8e00792e97140f228e3122515aa5
Xailus <chrisseiler96@gmail.com>
2019-06-04 19:45:04 -0400

    finished

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,15 +2,34 @@
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
-    
+    for i in range(len(merged_arr)):
+        print("i=",i)
+        print(arrA, arrB)
+        if len(arrA) > 0 and len(arrB) > 0:
+            if arrA[0] <= arrB[0]:
+                merged_arr[i] = arrA[0]
+                del arrA[0]
+            else:
+                merged_arr[i] = arrB[0]
+                del arrB[0]
+        elif len(arrA)>0:
+            merged_arr[i] = arrA[0]
+            del arrA[0]
+        else:
+            merged_arr[i] = arrB[0]
+            del arrB[0]
+
+    print(merged_arr)
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
-
+    length = len(arr)
+    if length > 1:
+        half = length//2
+        merged = merge(merge_sort(arr[:half]), merge_sort(arr[half:]))
+        arr = merged
     return arr
 
 
